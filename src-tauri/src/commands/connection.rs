946dd3fe650aac8ee9bf0b408814145d8884f8b8
everpcpc <git@everpcpc.com>
2026-01-27 15:14:00 +0800

    chore: update icon

--- src-tauri/src/commands/connection.rs
+++ src-tauri/src/commands/connection.rs
@@ -2,6 +2,7 @@
 
 use crate::app_state::{AppState, ConnectionStatusEvent};
 use crate::network::connection::Connection;
+use crate::network::messages::{HelloMessage, ProtocolMessage, RoomInfo, ClientFeatures};
 use std::sync::Arc;
 use tauri::State;
 
@@ -11,7 +12,7 @@ pub async fn connect_to_server(
     port: u16,
     username: String,
     room: String,
-    _password: Option<String>,
+    password: Option<String>,
     state: State<'_, Arc<AppState>>,
 ) -> Result<(), String> {
     tracing::info!(
@@ -32,8 +33,37 @@ pub async fn connect_to_server(
 
     // Connect to server
     match connection.connect(host.clone(), port).await {
-        Ok(_receiver) => {
+        Ok(mut receiver) => {
             tracing::info!("Successfully connected to server");
+
+            // Send Hello message
+            let hello = ProtocolMessage::Hello {
+                Hello: HelloMessage {
+                    username: username.clone(),
+                    password: password.clone(),
+                    room: Some(RoomInfo {
+                        name: room.clone(),
+                        password: None,
+                    }),
+                    version: "1.7.0".to_string(),
+                    realversion: "syncplay-tauri-0.1.0".to_string(),
+                    features: Some(ClientFeatures {
+                        shared_playlists: Some(true),
+                        chat: Some(true),
+                        ready_state: Some(true),
+                        managed_rooms: Some(false),
+                        persistent_rooms: Some(false),
+                    }),
+                    motd: None,
+                },
+            };
+
+            if let Err(e) = connection.send(hello) {
+                tracing::error!("Failed to send Hello message: {}", e);
+                return Err(format!("Failed to send Hello message: {}", e));
+            }
+
+            tracing::info!("Sent Hello message");
 
             // Store connection
             *state.connection.lock() = Some(connection);
@@ -51,14 +81,69 @@ pub async fn connect_to_server(
                 },
             );
 
-            // TODO: Send Hello message
-            // TODO: Start message processing loop
+            // Spawn message processing task
+            let state_clone = state.inner().clone();
+            tokio::spawn(async move {
+                while let Some(message) = receiver.recv().await {
+                    tracing::debug!("Received message: {:?}", message);
+                    handle_server_message(message, &state_clone).await;
+                }
+                tracing::info!("Message processing loop ended");
+            });
 
             Ok(())
         }
         Err(e) => {
             tracing::error!("Failed to connect: {}", e);
             Err(format!("Connection failed: {}", e))
+        }
+    }
+}
+
+async fn handle_server_message(message: ProtocolMessage, state: &Arc<AppState>) {
+    match message {
+        ProtocolMessage::List { List } => {
+            tracing::info!("Received user list: {:?}", List);
+            if let Some(users_by_room) = List {
+                // Transform nested HashMap into flat array of users
+                let mut users = Vec::new();
+                for (room_name, room_users) in users_by_room {
+                    for (username, user_info) in room_users {
+                        users.push(serde_json::json!({
+                            "username": username,
+                            "room": room_name,
+                            "file": user_info.file.and_then(|f| f.name),
+                            "isReady": user_info.is_ready.unwrap_or(false),
+                            "isController": user_info.controller.unwrap_or(false),
+                        }));
+                    }
+                }
+                tracing::info!("Transformed user list: {:?}", users);
+                // Emit user list event
+                state.emit_event("user-list-updated", serde_json::json!({ "users": users }));
+            }
+        }
+        ProtocolMessage::Chat { Chat } => {
+            tracing::info!("Received chat message: {:?}", Chat);
+            // Transform chat message to match frontend format
+            let chat_msg = serde_json::json!({
+                "timestamp": chrono::Utc::now().to_rfc3339(),
+                "username": Chat.username,
+                "message": Chat.message,
+                "messageType": "normal",
+            });
+            state.emit_event("chat-message-received", chat_msg);
+        }
+        ProtocolMessage::State { State: state_msg } => {
+            tracing::info!("Received state update: {:?}", state_msg);
+            // TODO: Handle state update
+        }
+        ProtocolMessage::Error { Error } => {
+            tracing::error!("Received error from server: {:?}", Error);
+            // TODO: Handle error
+        }
+        _ => {
+            tracing::debug!("Received other message type");
         }
     }
 }
